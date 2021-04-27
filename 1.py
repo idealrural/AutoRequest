0354be82ac14f95891c785705d5e03f427f323ba
Liang Han <liang.han@orange.com>
2021-04-27 11:27:17 +0800

    print token

--- 1.py
+++ 1.py
@@ -27,6 +27,7 @@ def gettoken(refresh_token):
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     refresh_token = jsontxt['refresh_token']
+    print("refresh_token"+ str(refresh_token))
     access_token = jsontxt['access_token']
     with open(path, 'w+') as f:
         f.write(refresh_token)
